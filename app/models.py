c7d18cd97dd4836a8f1b9ac0fad5dede82ef0639
Wisdom Achor <wisdom8achor24@gmail.com>
2025-08-03 19:59:06 +0100

    Changed the password storage lenght in user model from 50 to 500 so it can accommodate flask werkzeug security hashing method.

--- app/models.py
+++ app/models.py
@@ -49,7 +49,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), nullable=False)
     email = db.Column(db.String(50), nullable=False, unique=True)
-    password = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(500), nullable=False)
     date_joined = db.Column(db.DateTime, default=datetime.today())
 
     likes = db.relationship('Like', backref='users', cascade='all, delete')

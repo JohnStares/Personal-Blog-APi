24253b29d2ddb768449a7b166b55a8b53e125f42
Wisdom Achor <wisdom8achor24@gmail.com>
2025-08-03 19:56:58 +0100

    Integrated a real database ran locally for testing and devekopment.

--- config.py
+++ config.py
@@ -5,12 +5,15 @@ import os
 
 load_dotenv()
 
+
+database_password = os.getenv("DATABASE_PASSWORD")
+
 class Config(object):
     DEBUG = False
     TESTING = False
     SECRET_KEY = os.getenv("SECRET_KEY")
     secret_key = os.getenv("secret_key")
-    SQLALCHEMY_DATABASE_URI = "sqlite:///blog.db"
+    SQLALCHEMY_DATABASE_URI = f"mysql://root:{database_password}@localhost:3306/blog"
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
     JWT_BLACKLIST_ENABLED = True
     JWT_BLACKLIST_TOKEN_CHECKS = ["access", "refresh"]

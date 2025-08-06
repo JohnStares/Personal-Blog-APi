16099f4985e8c82983059e440d1b0d1557b15664
Wisdom Achor <wisdom8achor24@gmail.com>
2025-08-06 04:28:03 +0100

    Added path for images and formats for images also.

--- config.py
+++ config.py
@@ -13,6 +13,8 @@ class Config(object):
     TESTING = False
     SECRET_KEY = os.getenv("SECRET_KEY")
     secret_key = os.getenv("secret_key")
+    UPLOAD_EXTENSIONS = ["jpg", "png", "gif", "ai", "tiff", "psd", "esp", "svg", "pdf", "jpeg"]
+    UPLOAD_PATH = "uploaded_images"
     SQLALCHEMY_DATABASE_URI = f"mysql://root:{database_password}@localhost:3306/blog"
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
     JWT_BLACKLIST_ENABLED = True
